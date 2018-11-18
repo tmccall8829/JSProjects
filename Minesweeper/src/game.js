9f26238e410d3658e561960b623844f15f5a8bae
Thomas McCall <thomas.e.mccall@gmail.com>
2018-11-18 12:30:20 -0600

    added game instructions

--- Minesweeper/src/game.js
+++ Minesweeper/src/game.js
@@ -1,3 +1,13 @@
+// To play Minesweeper, we will create instances of MineSweeperGame in command line.
+// For example:
+// In the command line, navigate to the lib directory and run `node`
+// Run `.load game.js` to load the contents of this file.
+// Then create a Game instance and run commands like so:
+// let game = new Game(3, 3, 3);
+// game.playMove(0, 1);
+// game.playMove(1, 2);
+// When done run `.exit`
+
 import { Board } from './board.js';
 
 class Game {
